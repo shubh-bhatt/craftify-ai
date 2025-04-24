d8e0fe5ddcd3ca6c498e22c56418da0dd40b725d
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-04-24 21:20:54 +0530

    minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 const inter = Outfit({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "CreatorAI",
+  title: "Craftify",
   description: "AI Content Generator",
 };
 
